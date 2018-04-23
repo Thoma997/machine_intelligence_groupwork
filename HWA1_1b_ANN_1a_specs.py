14d88d55faa81e39944c7bcd6188460280806dfc
biehlerM <37422461+biehlerM@users.noreply.github.com>
2018-04-23 14:25:54 +1000

    Final model for HWA1_1b

---
+++ HWA1_1b_ANN_1a_specs.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 22 12:50:22 2018
+
+@author: MichaelBiehler
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('HWA1_1b_Dataset.csv')
+train_X = dataset.iloc[:, [0, 1]].values
+train_y = dataset.iloc[:, 2].values
+
+
+
+import keras
+import tensorflow
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import RMSprop
+from keras.optimizers import Adam
+import keras
+
+
+mlp = Sequential()
+mlp.add(Dense(16, activation='relu', input_shape=(2,)))
+mlp.add(Dense(16, activation='relu'))
+mlp.add(Dense(16, activation='relu'))
+mlp.add(Dense(16, activation='relu'))
+mlp.add(Dense(16, activation='tanh'))
+mlp.add(Dense(16, activation='tanh'))
+mlp.add(Dense(16, activation='tanh'))
+mlp.add(Dense(16, activation='tanh'))
+mlp.add(Dense(16, activation='tanh'))
+mlp.add(Dense(16, activation='tanh'))
+mlp.add(Dense(16, activation='tanh'))
+mlp.add(Dense(16, activation='relu'))
+mlp.add(Dense(16, activation='relu'))
+mlp.add(Dense(16, activation='relu'))
+mlp.add(Dense(16, activation='relu'))
+mlp.add(Dense(1, activation='sigmoid'))
+mlp.compile(loss='binary_crossentropy', optimizer=RMSprop(),
+              metrics=['accuracy'])
+ mlpFit = mlp.fit(train_X, train_y, epochs=40000, verbose=0)
+score = mlp.evaluate(train_X, train_y)
+print(score)
+from matplotlib import pyplot
+pyplot.plot(mlpFit.history['acc'])
+pyplot.show()
+
+
+
+
+# Visualising the classifying results
+from matplotlib.colors import ListedColormap
+X_set, y_set = train_X, train_y
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, mlp.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('ANN Two Spirals Task 1b;Network specifications from 1a')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.legend()
+plt.show()
+
+
